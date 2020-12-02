583bd97d67eedaf5711ff744f029ed51e1ee4734
Andres Janes <andres.janes@windsor-telecom.co.uk>
2020-12-02 08:45:24 +0000

    Fixed styleguidist Vuetify setup

--- styleguidist/global.requires.js
+++ styleguidist/global.requires.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
+import Vuetify from 'vuetify/lib'
+import {opts} from '../src/plugins/vuetify'
 
-Vue.use(Vuetify, {iconFont: 'md'})
+Vue.use(Vuetify, opts)
